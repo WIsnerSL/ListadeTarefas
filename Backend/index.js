28f319715a3043d5f1ba79e2cd0ef18586ec26ee
WisnerSl <wisboni6@hotmail.com>
2024-10-31 16:22:06 -0300

    Backend finalizado

--- Backend/index.js
+++ Backend/index.js
@@ -120,8 +120,3 @@ app.listen(PORT, (err) => {
         console.log(`Servidor rodando em http://localhost:${PORT}`);
     }
 });
-
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
