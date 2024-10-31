24fa3b7bd86b16560c31c5113dce735be08d5965
WisnerSl <wisboni6@hotmail.com>
2024-10-30 22:45:10 -0300

    enviar server

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
